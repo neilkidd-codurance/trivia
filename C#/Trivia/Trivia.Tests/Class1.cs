cc98f79f7a124b756c5682691f9880de3085045d
neilkidd-codurance <neil.kidd@codurance.com>
2017-11-21 18:04:06 +0000

    configure a test project with a passing test. Iteration zero?

---
+++ C#/Trivia/Trivia.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Trivia.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void FirstTest()
+        {
+            Assert.That(true, Is.EqualTo(true));
+        }
+    }
+}
